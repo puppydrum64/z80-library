97b1f5d823e08afbd6c903a6d3c1dc16c54a469b
puppydrum64 <88694683+puppydrum64@users.noreply.github.com>
2021-10-10 22:21:16 -0400

    Update RLD_RRD.asm
    Added an explanation for how to achieve gbz80 compatibility

--- RLD_RRD.asm
+++ RLD_RRD.asm
@@ -1,11 +1,11 @@
 ;File		RLD_RRD.ASM	(formerly named "shift_rotate.asm")
 ;Version	V2.0
 ;Date		October 10,2021
-; 			THESE DO NOT WORK ON GAME BOY OR GAME BOY COLOR
+; 		THESE DO NOT WORK ON GAME BOY OR GAME BOY COLOR, UNLESS YOU CALL THE SUBROUTINES IN "GB_RLD_RRD.ASM" IN PLACE OF THE "RLD" AND "RRD" IN THESE ROUTINES.
 
 ;Content	RRCA_RANGE - rotates a range of memory, e.g. &1000 = &23,&45,&67,&89 > &89,&23,&45,&67
-;			RLCA_RANGE - rotates a range of memory, e.g. &1000 = &23,&45,&67,&89 > &45,&67,&89,&23	(bugged, doesn't work)
-;			ShowPackedBCD_FromHL - Displays (HL) as a packed BCD byte to the screen. Non-destructive.
+;		RLCA_RANGE - rotates a range of memory, e.g. &1000 = &23,&45,&67,&89 > &45,&67,&89,&23	(bugged, doesn't work)
+;		ShowPackedBCD_FromHL - Displays (HL) as a packed BCD byte to the screen. Non-destructive.
 ;Sample Usage
 
 ;	ld hl,&1000	(addr of 0th byte in range)
@@ -23,9 +23,9 @@ RRCA_RANGE:	;DOESN'T WORK ON GAME BOY
 	push hl
 	push bc
 loop_RRCA_RANGE_FIRST:
-		RRD
+		RRD			   ;ON GAMEBOY, USE "CALL GB_RRD" (SEE GB_RLD_RRD.ASM)
 		INC HL
-		z_djnz loop_RRCA_RANGE_FIRST
+		DJNZ loop_RRCA_RANGE_FIRST ;ON GAMEBOY, USE "DEC B JR NZ, loop_RRCA_RANGE_FIRST"
 	pop bc
 	pop hl
 	push hl
@@ -33,20 +33,20 @@ loop_RRCA_RANGE_FIRST:
 loop_RRCA_RANGE_SECOND:
 		RRD
 		INC HL
-		z_djnz loop_RRCA_RANGE_SECOND
+		DJNZ loop_RRCA_RANGE_SECOND
 	pop bc
 	pop hl
 	RRD
 	ret
 	
-RLCA_RANGE:		;DOESN'T WORK ON GAME BOY
+RLCA_RANGE:		
 ;rotates a range of memory, e.g. &1000 = &23,&45,&67,&89 > &45,&67,&89,&23
 ;point HL at the end of the memory range this time.
 	ld a,(hl)
 	push hl
 	push bc
 loop_RLCA_RANGE_FIRST:
-		RLD
+		RLD				;ON GAMEBOY, USE "CALL GB_RLD" (SEE GB_RLD_RRD.ASM)
 		DEC HL
 		z_djnz loop_RLCA_RANGE_FIRST
 	pop bc
